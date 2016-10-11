7662c54c087e4608cfe9d1d81985da3ab6625101
jmuth <joachim.h.muth@gmail.com>
2016-10-11 12:52:04 +0200

    fill up py scripts

--- labs/ex04/template/least_squares.py
+++ labs/ex04/template/least_squares.py
@@ -7,11 +7,39 @@ Least Square
 import numpy as np
 
 
+def compute_e(y, tx, w):
+    ''' Computation of e according to the lesson week 2
+    '''
+    return (y - np.dot(tx,w))
+
+def compute_N(e):
+    ''' Size of e for normalization (theory lesson week 2)
+    '''
+    return e.shape[0]
+
+def compute_cost(y, tx, w):
+    """calculate the cost.
+
+    you can calculate the cost by mse or mae.
+    """
+    e = compute_e(y, tx, w)                      # vector e
+    N = compute_N(e)                             # size
+    L_MSE = np.dot(np.matrix.transpose(e), e)
+    L_MSE = L_MSE / (2 * N)                      # normalization
+    
+    return L_MSE
+
 def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    """calculate the least squares solution."""
+    xx = np.dot(np.matrix.transpose(tx), tx)
+    
+    # handle non-inversable matrix case
+    try:
+        inv_xx = np.linalg.inv(xx)
+    except: 
+        raise ValueError('Matrix xx is not invertible')
+    
+    w = np.matrix.dot(np.matrix.dot(inv_xx, np.matrix.transpose(tx)), y)
+    loss = compute_cost(y, tx, w)
+    
+    return loss, w
--- labs/ex04/template/ridge_regression.py
+++ labs/ex04/template/ridge_regression.py
@@ -6,11 +6,24 @@ Ridge Regression
 
 import numpy as np
 
-
 def ridge_regression(y, tx, lamb):
     """implement ridge regression."""
     # ***************************************************
     # INSERT YOUR CODE HERE
     # ridge regression: TODO
     # ***************************************************
-    raise NotImplementedError
+    
+    # Compute optimal weights
+    xx = np.dot(np.transpose(tx),tx)
+
+    bxx = xx + lamb*np.identity(len(xx))
+
+    try:
+        inv = np.linalg.inv(bxx)
+    except:
+        raise ValueError("Matrix X^TX not invertible") 
+
+    xy = np.dot(np.transpose(tx),y)
+    w_star = np.dot(inv, xy)
+   
+    return w_star
